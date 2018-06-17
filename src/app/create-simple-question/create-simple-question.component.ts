bd6a5a6fcf14964ab0c63e4898110138d2abd122
farzin.khoshneyat <farzin.khoshneyat@student.fh-kiel.de>
2018-06-17 02:56:06 +0200

    simple question basic question code validation

--- src/app/create-simple-question/create-simple-question.component.ts
+++ src/app/create-simple-question/create-simple-question.component.ts
@@ -2,24 +2,24 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import * as CodeMirror from 'codemirror/lib/codemirror'
 
 import 'codemirror/mode/javascript/javascript'
-@Component({
-  selector: 'app-create-simple-question',
-  templateUrl: './create-simple-question.component.html',
-  styleUrls: ['./create-simple-question.component.css']
-})
+@Component({ selector: 'app-create-simple-question', templateUrl: './create-simple-question.component.html', styleUrls: ['./create-simple-question.component.css'] })
 export class CreateSimpleQuestionComponent implements OnInit {
   theme: string = "default"
   c: any;
   @ViewChild('editor') editor: ElementRef
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
   ngAfterViewInit() {
     this.c = CodeMirror.fromTextArea(this.editor.nativeElement, {
       lineNumbers: true,
-      extraKeys: { "Ctrl-Space": "autocomplete" },
-      mode: { name: "javascript", globalVars: true },
+      extraKeys: {
+        "Ctrl-Space": "autocomplete"
+      },
+      mode: {
+        name: "javascript",
+        globalVars: true
+      },
       styleActiveLine: true,
       matchBrackets: true
     });
@@ -28,15 +28,39 @@ export class CreateSimpleQuestionComponent implements OnInit {
     if (this.c.getOption("theme") === "default")
       this.c.setOption("theme", "ambiance")
     else
-      this.c.setOption("theme", "default")
+      this.c
+        .setOption("theme", "default")
   }
-  verify(){
-    var x = { "func": 2}
-    console.log(JSON.stringify(x))
-     //var x = JSON.parse(JSON.stringify(f))
-    //console.log(x)
-    //if(eval(this.c.getValue())=== undefined)
-    //console.log("ok")
+  verify() {
+
+//this should be defined by supervisor as the name of the main function
+//which user should at least define in his/her code
+    let fun = "calc"
+    if (eval(this.c.getValue()) === undefined && this.c.getValue() !== "") {
+      console.log("ok")
+      let name = "testarea"
+      if (document.getElementById(name)){
+        document.getElementById(name).remove()
+        
+        // @ts-ignore
+        calc = ""
+      }
+      var sc = document.createElement("script")
+      sc.id = name
+      sc.type = "text/javascript";
+      sc.innerHTML = this.c.getValue()
+      document.getElementsByTagName("head")[0].appendChild(sc);
+      try{
+        // @ts-ignore
+      console.log(calc())
+      }
+      catch{
+        console.log("wrong")
+      }
+    }
+
+
+
   }
 
 }
