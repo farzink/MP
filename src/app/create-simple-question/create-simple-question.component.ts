876ca5ea7c02a257171bb49d8a219c4f103d0ff1
farzin.khoshneyat <farzin.khoshneyat@student.fh-kiel.de>
2018-06-19 03:29:52 +0200

    rule modifications

--- src/app/create-simple-question/create-simple-question.component.ts
+++ src/app/create-simple-question/create-simple-question.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import * as CodeMirror from 'codemirror/lib/codemirror'
-
+import * as UIKit from 'uikit/dist/js/uikit.min.js'
 import 'codemirror/mode/javascript/javascript'
-@Component({ selector: 'app-create-simple-question', templateUrl: './create-simple-question.component.html', styleUrls: ['./create-simple-question.component.css'] })
+@Component({ selector: 'app-create-simple-question', templateUrl: './create-simple-question.component.html', styleUrls: ['create-simple-question.component.scss'] })
 export class CreateSimpleQuestionComponent implements OnInit {
   theme: string = "default"
   c: any;
   @ViewChild('editor') editor: ElementRef
-  constructor() { }
+  @ViewChild('entryPoint') entryPoint: ElementRef
+  @ViewChild('resultHtml') resultHtml: ElementRef
+  @ViewChild('parameters') parameters: ElementRef
 
+
+  constructor() { }
+  hardness = "EASY"
   ngOnInit() { }
   ngAfterViewInit() {
     this.c = CodeMirror.fromTextArea(this.editor.nativeElement, {
@@ -33,34 +38,52 @@ export class CreateSimpleQuestionComponent implements OnInit {
   }
   verify() {
 
-//this should be defined by supervisor as the name of the main function
-//which user should at least define in his/her code
-    let fun = "calc"
+    //this should be defined by supervisor as the name of the main function
+    //which user should at least define in his/her code
+    //let fun = "calc"
     if (eval(this.c.getValue()) === undefined && this.c.getValue() !== "") {
       console.log("ok")
       let name = "testarea"
-      if (document.getElementById(name)){
+      if (document.getElementById(name)) {
         document.getElementById(name).remove()
-        
+
         // @ts-ignore
-        calc = ""
+        //  calc = ""
       }
       var sc = document.createElement("script")
       sc.id = name
       sc.type = "text/javascript";
       sc.innerHTML = this.c.getValue()
       document.getElementsByTagName("head")[0].appendChild(sc);
-      try{
+      try {
         // @ts-ignore
-      console.log(calc())
+        if (this.entryPoint !== "") {
+          let params = this.parameters.nativeElement.value.split(",")
+            .map(e => parseInt(e.trim()))
+          let func =window[this.entryPoint.nativeElement.value]
+          // for (var i = 0; i < params.length; i++) {
+          //   func.arguments[i] = params[i]
+          // }
+          this.resultHtml.nativeElement.innerHTML = func(...params)
+        }
       }
       catch{
-        console.log("wrong")
+        UIKit.notification({ message: 'entrypoint should be provided', status: 'danger' })
       }
     }
 
 
 
+  }
+
+  setHardness(value) {
+    if (value === 1) {
+      this.hardness = "EASY"
+    } else if (value === 2) {
+      this.hardness = "MEDIUM"
+    } else if (value === 3) {
+      this.hardness = "HARD"
+    }
   }
 
 }
